151396c2d699113af6ab9493d005cebee2566845
raymkindo <raymkindo02@gmail.com>
2024-04-21 12:31:03 +0300

    Login UPdates

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,96 @@
+  <!-- <section> begin ============================-->
+    <section class="bg-body-highlight dark__bg-gray-1100 py-9">
+        <div class="container-small">
+            <div class="row justify-content-between gy-4">
+                <div class="col-12 col-lg-4">
+                    <div class="d-flex align-items-center mb-3"><img src="assets/img/icons/logo.png"
+                            alt="phoenix" width="27" />
+                        <p class="logo-text ms-2">phoenix</p>
+                    </div>
+                    <p class="text-body-tertiary mb-1 fw-semibold lh-sm fs-9">Phoenix is an admin dashboard
+                        template with fascinating features and amazing layout. The template is responsive to all
+                        major browsers and is compatible with all available devices and screen sizes.</p>
+                </div>
+                <div class="col-6 col-md-auto">
+                    <h5 class="fw-bolder mb-3">About Phoenix</h5>
+                    <div class="d-flex flex-column"><a class="text-body-tertiary fw-semibold fs-9 mb-1"
+                            href="#!">Careers</a><a class="text-body-tertiary fw-semibold fs-9 mb-1"
+                            href="#!">Affiliate Program</a><a
+                            class="text-body-tertiary fw-semibold fs-9 mb-1" href="#!">Privacy Policy</a><a
+                            class="text-body-tertiary fw-semibold fs-9 mb-1" href="#!">Terms &
+                            Conditions</a></div>
+                </div>
+                <div class="col-6 col-md-auto">
+                    <h5 class="fw-bolder mb-3">Stay Connected</h5>
+                    <div class="d-flex flex-column"><a class="text-body-tertiary fw-semibold fs-9 mb-1"
+                            href="#!">Blogs</a><a class="mb-1 fw-semibold fs-9 d-flex"
+                            href="#!"><span
+                                class="fab fa-facebook-square text-primary me-2 fs-8"></span><span
+                                class="text-body-secondary">Facebook</span></a><a
+                            class="mb-1 fw-semibold fs-9 d-flex" href="#!"><span
+                                class="fab fa-twitter-square text-info me-2 fs-8"></span><span
+                                class="text-body-secondary">Twitter</span></a></div>
+                </div>
+                <div class="col-6 col-md-auto">
+                    <h5 class="fw-bolder mb-3">Customer Service</h5>
+                    <div class="d-flex flex-column"><a class="text-body-tertiary fw-semibold fs-9 mb-1"
+                            href="#!">Help Desk</a><a class="text-body-tertiary fw-semibold fs-9 mb-1"
+                            href="#!">Support, 24/7</a><a class="text-body-tertiary fw-semibold fs-9 mb-1"
+                            href="#!">Community of Phoenix</a></div>
+                </div>
+                <div class="col-6 col-md-auto">
+                    <h5 class="fw-bolder mb-3">Payment Method</h5>
+                    <div class="d-flex flex-column"><a class="text-body-tertiary fw-semibold fs-9 mb-1"
+                            href="#!">Cash on Delivery</a><a
+                            class="text-body-tertiary fw-semibold fs-9 mb-1" href="#!">Online Payment</a><a
+                            class="text-body-tertiary fw-semibold fs-9 mb-1" href="#!">PayPal</a><a
+                            class="text-body-tertiary fw-semibold fs-9 mb-1" href="#!">Installment</a>
+                    </div>
+                </div>
+            </div>
+        </div><!-- end of .container-->
+    </section><!-- <section> close ============================-->
+    <!-- ============================================-->
+
+    <footer class="footer position-relative">
+        <div class="row g-0 justify-content-between align-items-center h-100">
+            <div class="col-12 col-sm-auto text-center">
+                <p class="mb-0 mt-2 mt-sm-0 text-body">Eshop Classified Ads<span
+                        class="d-none d-sm-inline-block"></span><span
+                        class="d-none d-sm-inline-block mx-1">|</span><br class="d-sm-none" />2024 &copy;<a
+                        class="mx-1" href="https://qvcafrica.com/">QVC AFRICA</a></p>
+            </div>
+            <div class="col-12 col-sm-auto text-center">
+                <p class="mb-0 text-body-tertiary text-opacity-85">v1.01.0</p>
+            </div>
+        </div>
+    </footer>
+</main><!-- ===============================================-->
+<!--    End of Main Content-->
+<!-- ===============================================-->
+
+</a>
+
+<!-- ===============================================-->
+<!--    JavaScripts-->
+<!-- ===============================================-->
+<script src="assets/vendors/popper/popper.min.js"></script>
+<script src="assets/vendors/bootstrap/bootstrap.min.js"></script>
+<script src="assets/vendors/anchorjs/anchor.min.js"></script>
+<script src="assets/vendors/is/is.min.js"></script>
+<script src="assets/vendors/fontawesome/all.min.js"></script>
+<script src="assets/vendors/lodash/lodash.min.js"></script>
+<script src="assets/polyfill.io/v3/polyfill.min58be.js?features=window.scroll"></script>
+<script src="assets/vendors/list.js/list.min.js"></script>
+<script src="assets/vendors/feather-icons/feather.min.js"></script>
+<script src="assets/vendors/dayjs/dayjs.min.js"></script>
+<script src="assets/js/phoenix.js"></script>
+<script src="assets/vendors/swiper/swiper-bundle.min.js"></script>
+
+
+</body>
+
+
+<!-- Mirrored from prium.github.io/phoenix/v1.14.0/apps/e-commerce/landing/homepage.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 29 Jan 2024 13:56:46 GMT -->
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,31 @@
 <?php
 
+use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
+
+
+// HomePage For The Websites
+Route::get('/', [HomepageController::class, 'index'])->name('index');
+
+Route::middleware(['auth'])->group(function () {
+    Route::view('/seller/profile', 'seller.profile')->name('seller.profile');
+    // Other routes...
+});
